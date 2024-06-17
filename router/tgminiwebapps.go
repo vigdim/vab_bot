4b25789c01ad8082c8b358ba733378dab861e83a
Sergey Karpov <7150222-karpoffs@users.noreply.gitlab.com>
2024-06-17 19:22:10 +0300

    refactoring web views

---
+++ router/tgminiwebapps.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Привет!")
+}
+
+//func NewRouter() http.Handler {
+//	router := chi.NewRouter()
+//	r.Get("/{name}", HelloName)
+//
+//	// Настройка раздачи статических файлов
+//	staticPath, _ := filepath.Abs("views/pages/")
+//	fs := http.FileServer(http.Dir(staticPath))
+//	router.Handle("/*", fs)
+//
+//	return r
+//}
