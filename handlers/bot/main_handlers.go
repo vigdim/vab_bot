b04bcb6089f76e940f06c778ec92c727189c5b4a
Sergey Karpov <7150222-karpoffs@users.noreply.gitlab.com>
2024-06-19 17:55:18 +0300

    change structure project

--- handlers/bot/main_handlers.go
+++ handlers/bot/main_handlers.go
@@ -1,4 +1,4 @@
-package main_handlers
+package bot
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/joho/godotenv" // Загрузка настроек из .env файла
-	"github.com/mymmrac/telego"
-	th "github.com/mymmrac/telego/telegohandler"
 	models "github.com/vigdim/vab_bot/database"
-	main_handlers "github.com/vigdim/vab_bot/handlers"
-	admin_handlers "github.com/vigdim/vab_bot/handlers/admin"
-	user_handlers "github.com/vigdim/vab_bot/handlers/user"
+	bot_router "github.com/vigdim/vab_bot/router/bot"
 	"log"
-	"os"
 )
 
 func main() {
@@ -18,74 +12,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Ошибка загрузки .env файла")
 	}
-	botToken := os.Getenv("TOKEN")
 
 	models.Init()
-
-	// Примечание. Средство ведения журнала по умолчанию может раскрывать конфиденциальную информацию.
-	// Использовать только в разработке
-	debugMode := os.Getenv("DEBUG_MODE")
-	printErrors := os.Getenv("PRINT_ERRORS")
-	var BotOption = telego.WithDefaultLogger((debugMode == "Develop"), (printErrors == "Yes"))
-
-	bot, err := telego.NewBot(botToken, BotOption)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// Get updates channel
-	updates, _ := bot.UpdatesViaLongPolling(nil)
-
-	// Create bot handler and specify from where to get updates
-	bh, _ := th.NewBotHandler(bot, updates)
-
-	// Stop handling updates
-	defer bh.Stop()
-
-	// Stop getting updates
-	defer bot.StopLongPolling()
-
-	// START HANDLERS BLOCK ///////////////////////////////////////////////////////////////////////
-	// user handlers message ==============================================================
-	// Переход в меню Каталог
-	bh.Handle(user_handlers.CatalogMess, th.TextEqual("📁 Каталог"))
-	// Переход в меню О компании
-	bh.Handle(user_handlers.AboutMess, th.TextEqual("©️О компании"))
-	// Переход в меню Каталог
-	bh.Handle(user_handlers.MainMenuMess, th.TextEqual("🔙 В НАЧАЛО"))
-	// Переход в меню Кабинет
-	bh.Handle(user_handlers.CabinetMess, th.TextEqual("👤 Кабинет"))
-	// Переход в меню Консультация
-	bh.Handle(user_handlers.ConsultationMess, th.TextEqual("💬 Консультация"))
-	// Переход в меню Купить ОФД (вывод списка ОФД)
-	bh.Handle(user_handlers.ListOfd, th.TextEqual("💵 Купить ОФД"))
-	// Переход в меню Заказы (вывод списка купленных ОФД кодов)
-	bh.Handle(user_handlers.OrdersMess, th.TextEqual("🧾 Заказы"))
-
-	// user handlers CallbackQuery =======================================================
-	// cback_ОФД
-	bh.HandleCallbackQuery(user_handlers.GetOneOfdCb, th.AnyCallbackQueryWithMessage(), th.CallbackDataPrefix("cb_OFD_"))
-	// cback Консультация
-	bh.HandleCallbackQuery(user_handlers.ConsultationCb, th.AnyCallbackQueryWithMessage(), th.CallbackDataPrefix("cb_cons_"))
-
-	// admin handlers message ==============================================================
-	// Переход в меню Каталог
-	bh.Handle(admin_handlers.MainUserMenuMess, th.TextEqual("👨‍💼Пользователь"))
-
-	// main_handlers ==============================================================================
-	bh.Handle(main_handlers.Start, th.CommandEqual("start"))    //Вывод приветствия
-	bh.Handle(main_handlers.SendMyData, th.TextEqual("telega")) //Вывод данных пользователя
-
-	// TODO: Clean code
-	//http.HandleFunc("/", sayhello)       // Устанавливаем роутер
-	//err = http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
-	//if err != nil {
-	//	log.Fatal("ListenAndServe: ", err)
-	//}
-	//http.HandleFunc("/", sayhello)
-	//go http.ListenAndServe(":80", nil)
-
-	// Start handling updates
-	bh.Start()
+	bot_router.Init()
 }
